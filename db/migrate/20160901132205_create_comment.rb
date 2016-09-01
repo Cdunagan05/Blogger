14960c4cb1a02506950e5dc0adc24d9683135c7b
Chase Dunagan <cdunagan@utexas.edu>
2016-09-01 08:20:23 -0600

    Done with iteration 2

---
+++ db/migrate/20160901132205_create_comment.rb
@@ -0,0 +1,12 @@
+class CreateComment < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+
+      t.string :author_name
+      t.text :body
+      t.references :article
+
+      t.timestamps null: false
+    end
+  end
+end
