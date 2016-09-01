c920d705b487b1c02b95bce2fe2e6aaee16b2901
Chase Dunagan <cdunagan@utexas.edu>
2016-08-31 20:35:11 -0600

    first blogger commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,27 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+end
