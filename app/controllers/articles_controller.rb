1708714c4ba6a811a5b2b2c97e7b462c6cdf5567
Chase Dunagan <cdunagan@utexas.edu>
2016-09-01 00:00:03 -0600

    Done with iteration 1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
 
   def index
     @articles = Article.all
   end
 
   def new
-
+    @article = Article.new
   end
 
   def show
@@ -13,15 +14,34 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
 
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
 
+    flash.notice = "Article '#{@article.title}' has been Created!"
+
+    redirect_to article_path(@article)
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
 
+    flash.notice = "Article '#{@article.title}' has been Deleted!!"
+
+    redirect_to articles_path
   end
 
 end
